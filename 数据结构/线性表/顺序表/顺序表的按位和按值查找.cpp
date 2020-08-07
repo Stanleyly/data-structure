f87b04fb4ef2dd79e17fb2d9f3ab7b6cb454f77a
Stanleyly <1963785534@qq.com>
2020-08-07 18:00:51 +0800

    数据结构

---
+++ 数据结构/线性表/顺序表/顺序表的按位和按值查找.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdio> 
+#include<cstring>
+using namespace std;
+
+#define MaxSize 20
+
+typedef struct{
+	int data[MaxSize];
+	int length;
+}SeqList; 
+
+void InitList(SeqList &L)
+{
+	for(int i=0;i<MaxSize;++i)
+	{
+		L.data[i]=0;
+	 } 
+	L.length=0;
+} 
+
+int GetElem(SeqList &L, int i)//L中第i位元素值 
+{
+	return L.data[i-1];
+}
+
+int LocateElem(SeqList &L,int e)//L中值为e的位置在那 
+{
+	for(int i=0;i<L.length;++i)
+	{
+		if(e == L.data[i])
+		return i+1;
+	}
+	return 0;
+}
+
+
+int main(void)
+{
+	SeqList L;
+	InitList(L);
+	GetElem(L,2);
+	LocateElem(L,3);
+	return 0;
+}
+
