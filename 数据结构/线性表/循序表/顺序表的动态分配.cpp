2472abc07c319a555c55d01e2c4fa211baee8758
Stanleyly <1963785534@qq.com>
2020-08-07 15:34:53 +0800

    数据结构

---
+++ 数据结构/线性表/循序表/顺序表的动态分配.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+
+#define InitSize 10
+
+typedef struct{
+	int *data;//动态分配数组指针 
+	int MaxSize;//顺序表最大容量 
+	int length;//顺序表当前长度 
+}SeqList; 
+
+void InitList(SeqList &L)
+{
+	L.data = (int *) malloc(sizeof(int) * InitSize);
+	L.length = 0;
+	L.MaxSize = InitSize;
+}
+
+void IncreaseSize(SeqList &L,int len)
+{
+	int *q = L.data;
+	L.data = (int *) malloc(sizeof(int) * (InitSize + len));
+	for(int i = 0;i < L.length;++i)
+	{
+		L.data[i] = q[i];//将数据复制到新区域 
+	}
+	L.MaxSize = L.MaxSize + len;  //顺序表最大容量增加len 
+	free(q);
+ } 
+ 
+int main(void)
+{
+	SeqList L;
+	InitList(L);
+	IncreaseSize(L,5);
+	return 0;
+ } 
